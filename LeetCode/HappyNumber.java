7a22d7037b647e54621a9ee1de206fa8731566d9
Dexter <vivekojha961@gmail.com>
2020-04-18 15:34:20 -0500

    leetcode

---
+++ LeetCode/HappyNumber.java
@@ -0,0 +1,32 @@
+
+public class HappyNumber {
+    public static void main(String []args){
+        
+        
+            System.out.print(isHappy(19));
+
+    }
+    public static boolean isHappy(int n) {
+        int arr[]= new int[10000];
+        int sum =0;
+        int i=0;
+        while(true){
+            while(n>0)
+            {
+            n = n%10;
+            arr[i] = n;
+            n = n/10;
+                i++;
+            }
+            for(int j:arr){
+                sum = sum + (arr[j]*arr[j]);
+            }
+            if(sum == 1)
+                return true;
+            else 
+                n = sum;
+            
+        }
+    }
+}
+

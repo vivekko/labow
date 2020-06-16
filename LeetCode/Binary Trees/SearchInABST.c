b24c2923dd3146bd3246ba92fafcae4c85fcab71
Dexter <vivekojha961@gmail.com>
2020-06-16 09:50:45 +0530

    Leetcode Jun 16

---
+++ LeetCode/Binary Trees/SearchInABST.c
@@ -0,0 +1,22 @@
+
+//  * Definition for a binary tree node.
+#include<stdlib.h>
+
+  struct TreeNode {
+      int val;
+      struct TreeNode *left;
+      struct TreeNode *right;
+  };
+struct TreeNode* searchBST(struct TreeNode* root, int vall){
+    if(root == NULL)
+        return NULL;
+    else{
+        if(root->val == vall)
+            return root;
+        else if(root->val>vall)
+            return searchBST(root->left,vall);
+        else 
+            return searchBST(root->right,vall);
+    }
+
+}

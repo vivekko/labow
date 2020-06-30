647ce086b059d6d71de234ad1408e150ce1b583c
Dexter <vivekojha961@gmail.com>
2020-06-30 21:35:56 +0530

    A20j 50 52 51

---
+++ Codeforces/A_The_number_of_positions.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n,a,b;
+    cin>>n>>a>>b;
+    cout<<(n-a);
+    return 0;
+}

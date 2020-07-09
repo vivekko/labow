b5b02bb83e51ef6cce6fb10b4725b6082f9eb80a
Dexter <vivekojha961@gmail.com>
2020-07-10 02:00:28 +0530

    A20J 56

---
+++ Codeforces/B_PresentFromLena.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    cin>>n;
+
+
+    for(int i=0;i<=n;i++)
+        for(int j=0;j<=n;j++)
+        {
+            
+        }
+    
+    return 0;
+}

ce465353baa6a87bdd6c2fb7dc27ef7e110fae10
Dexter <vivekojha961@gmail.com>
2020-07-15 12:46:55 +0530

    2

--- Basics of DP/PrintLCS.java
+++ Basics of DP/PrintLCS.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+// import java.util.Arrays;
 import java.util.Scanner;
 
 public class PrintLCS {
@@ -11,44 +11,39 @@ public class PrintLCS {
         String ans = printLcs(s1,s2,m,n);
         StringBuffer a = new StringBuffer(ans);
         System.out.println(a.reverse());
-
+        in.close();
     }
-
     private static String printLcs(String s1, String s2, int m, int n) {
-        String ret = new String();
         int dp[][] = new int[m+1][n+1];
         for(int i=0;i<=m;i++)
-            dp[i][0] = 0;
+        dp[i][0] = 0;
         for(int j=0;j<=n;j++)
-            dp[0][j] = 0;
+        dp[0][j] = 0;
         for(int i=1;i<=m;i++)
-            for(int j =1;j<=n;j++){
-                if(s1.charAt(i-1)==s2.charAt(j-1))
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                else
-                    {
-                        Math.max(dp[i-1][j], dp[i][j-1]);
-                    }
-                
+        for(int j =1;j<=n;j++){
+            if(s1.charAt(i-1)==s2.charAt(j-1))
+            dp[i][j] = 1 + dp[i-1][j-1];
+            else
+            {
+                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
             }
-
-// Printing LCS
-        // for(int )
-        int i = s1.length();
-        int j = s2.length();
+            
+        }
+        String ret = new String();
+        int i = m;
+        int j = n;
         while(i>0 && j>0){
             if(s1.charAt(i-1)==s2.charAt(j-1)){
                 ret = ret + s1.charAt(i-1);
-                --i;
-                --j;
+                i--;
+                j--;
             }
-            else{
-                if(dp[i][j-1]>dp[j][i-1])
-                    --j;
-                else
-                    --i;
+            else if(dp[i][i-1]>dp[j][j-1])
+                    i--;
+            else
+                    j--;
             }
-        }
+        
         return ret;
     }
 }

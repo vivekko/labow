0862f9e823178523fe84817e36eec2c32fa12155
Dexter <vivekojha961@gmail.com>
2020-09-08 13:50:43 +0530

    new codes

---
+++ AMonthOfCodeforces/hoiad.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+public class E {
+     public static void main(String[] args) {
+         FastScanner fs=new FastScanner();
+         int T=fs.nextInt();
+         for (int tt=0; tt<T; tt++) {
+                 int n = fs.nextInt();
+                
+                int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+        arr[i] = fs.nextInt();
+        }
+        for(int i=n-1;i>=0;i--){
+            cout<<arr[i]<<" ";
+    }
+    cout<<"\n";   
+                 }
+}
+ 
+ static class FastScanner {
+     BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+     StringTokenizer st=new StringTokenizer("");
+     String next() {
+     while (!st.hasMoreTokens())
+         try {
+             st=new StringTokenizer(br.readLine());
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+         return st.nextToken();
+     }
+     
+     int nextInt() {
+         return Integer.parseInt(next());
+     }
+     int[] readArray(int n) {
+         int[] a=new int[n];
+         for (int i=0; i<n; i++) a[i]=nextInt();
+         return a;
+     }
+     long nextLong() {
+         return Long.parseLong(next());
+     }
+ }
+
+ 
+}

d3fa6c41086e2ffdc26de61394d0e8dba1e5b6f5
Dexter <vivekojha961@gmail.com>
2020-09-16 23:47:04 +0530

    ..

---
+++ AMonthOfCodeforces/MaximalContinuousRest.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+void solve(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    if(arr[n-1] == 1){
+        for(int i=0;i<n;i++){
+            if(arr[i] == 1)
+                arr.push_back(arr[i]);
+            else
+                break;
+        }
+    }
+    int maxc = 0;
+    int maxg = 0;
+    // debug(arr);
+    for(int i=0;i<arr.size();i++){
+        if(arr[i] == 1)
+        {
+            maxc++;
+            maxg = max(maxc,maxg);
+        }
+        else{
+            maxc = 0;
+            maxg = max(maxc,maxg);
+        }
+    }
+    cout<<maxg; 
+
+}
+int main(){
+int t=1;
+// cin>>t;
+while(t-->0)
+    solve();
+return 0;
+}

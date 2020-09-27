a4066f0e59e0df5a1b92a262fbe040d2b2520830
Dexter <vivekojha961@gmail.com>
2020-09-27 11:24:57 +0530

    New codes

---
+++ AMonthOfCodeforces/Emotes.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+#define int long long
+#define vivek main
+void solve(){
+    int n,m,k;
+    int e;
+    cin>>n>>m>>k;
+    int arr[n];
+    int maxe = -1;
+    int maxn = -1;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        if(arr[i]>maxe){
+            maxe = arr[i];
+            e = i;
+        }
+    }
+
+    arr[e] = -1;
+
+    for(int i=0;i<n;i++){
+        if(arr[i]>maxn){
+            maxn = arr[i];
+            e = i;
+        }
+    }
+    int ans = 0;
+    int mul = m/(k+1);
+    int rem = m%(k+1);
+
+    ans = ans + mul*maxe*k;
+    ans = ans + mul*maxn;
+    ans = ans + rem*maxe;
+
+    cout<<ans;
+
+}
+int32_t vivek(){
+int t=1;
+// cin>>t;
+while(t-->0)
+    solve();
+return 0;
+}
+
+// ___.   .__                 __          .__              __    
+// \_ |__ |  | _____    ____ |  | _____  _|__|__  __ ____ |  | __
+//  | __ \|  | \__  \ _/ ___\|  |/ /\  \/ /  \  \/ // __ \|  |/ /
+//  | \_\ \  |__/ __ \   \___|    <  \   /|  |\   /\  ___/|    < 
+//  |___  /____(____  /\___  >__|_ \  \_/ |__| \_/  \___  >__|_ \
+//      \/          \/     \/     \/                    \/     \/

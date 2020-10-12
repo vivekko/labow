f2d3ef5fe47ae227c7e82f4ce94c5adb363b690f
Dexter <vivekojha961@gmail.com>
2020-10-12 13:29:39 +0530

    New codes

---
+++ Codeforces/1100/NumberOfApartments.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+#define int long long
+#define vivek main
+// vector<int> arr(3);
+bool flag=false;
+void solveit(int n,vector<int> arr,int start){
+    if(start == n){
+        flag = true;
+        for(int i=0;i<arr.size();i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<"\n";
+        return;
+    }
+    if(start > n){
+        return;
+    }
+    else{
+        arr[0]++;
+    solveit(n,arr,start+3);
+    arr[0]--;
+    if(!flag){
+        arr[1]++;
+    solveit(n,arr,start+5);
+    arr[1]--;
+    }
+    if(!flag){
+        arr[2]++;
+    solveit(n,arr,start+7);
+    arr[2]--;
+    }
+    }
+}
+
+void solve(){
+    int n;
+    cin>>n;
+    vector<int> arr(3);
+    solveit(n,arr,0);
+    if(flag == false){
+        cout<<-1<<endl;
+    }
+    flag = false;
+    return;
+
+}
+int32_t vivek(){
+int t;
+cin>>t;
+while(t-->0)
+    solve();
+return 0;
+}
+
+// ___.   .__                 __          .__              __    
+// \_ |__ |  | _____    ____ |  | _____  _|__|__  __ ____ |  | __
+//  | __ \|  | \__  \ _/ ___\|  |/ /\  \/ /  \  \/ // __ \|  |/ /
+//  | \_\ \  |__/ __ \   \___|    <  \   /|  |\   /\  ___/|    < 
+//  |___  /____(____  /\___  >__|_ \  \_/ |__| \_/  \___  >__|_ \
+//      \/          \/     \/     \/                    \/     \/

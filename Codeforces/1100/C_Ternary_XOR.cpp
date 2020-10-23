b22062e8d6073165498be088327edd3dbb2cbe90
Dexter <vivekojha961@gmail.com>
2020-10-23 23:33:46 +0530

    New Codes

---
+++ Codeforces/1100/C_Ternary_XOR.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+#define int long long
+#define vivek main
+void solve(){
+    int n;
+    cin>>n;
+    string val;
+    cin>>val;
+    string first = "1";
+    string second = "1";
+    bool flag = false;
+    for(int i=1;i<n;i++){
+        if(val[i] == '2'){
+            if(flag == false){
+            first+='1';
+            second+='1';
+            }
+            else{
+                first += '0';
+                second += '2';
+            }
+        }
+        else if(val[i] == '0'){
+            first+= '0';
+            second+= '0';
+        }
+        else{
+            if(flag == false){
+            first += '1';
+            second += '0';
+            flag = true;
+            }
+            else{
+                first += '0';
+                second += '1';
+            }
+        }
+    }
+    cout<<first<<"\n"<<second<<"\n";
+}
+int32_t vivek(){
+int t;
+cin>>t;
+while(t-->0)
+    solve();
+return 0;
+}
+
+// ___.   .__                 __          .__              __    
+// \_ |__ |  | _____    ____ |  | _____  _|__|__  __ ____ |  | __
+//  | __ \|  | \__  \ _/ ___\|  |/ /\  \/ /  \  \/ // __ \|  |/ /
+//  | \_\ \  |__/ __ \   \___|    <  \   /|  |\   /\  ___/|    < 
+//  |___  /____(____  /\___  >__|_ \  \_/ |__| \_/  \___  >__|_ \
+//      \/          \/     \/     \/                    \/     \/

bbd61d81fee3e44d15070764364fd4cdedd28578
Dexter <vivekojha961@gmail.com>
2020-10-26 22:01:56 +0530

    New Codes

---
+++ Codeforces/1200/C_Book_Reading.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+#define int long long
+#define vivek main
+void solve(){
+    int m,n;
+    cin>>m>>n;
+    int val = m/n;    
+    // cout<<val<<"\n";
+    int part1,part2;              
+    if(val%10 != 0){
+        part1 = val%10;
+        part2 = val - part1;
+    }
+    else{
+        part1 = 0;
+        part2 = val;
+    }
+    float factor = 0;
+    for(int i=0;i<10;i++){
+        factor += (i*n)%10;
+    }
+   
+    factor= (float)factor/10;
+    // cout<<"F "<<factor<<endl;
+    int ans = part2*(factor);
+
+    int i = 1;  
+    while(part1-->0){
+        int mul = n*i++;
+        // debug(mul%10);
+        ans+=mul%10;
+        
+    }
+
+cout<<ans<<endl;
+
+}
+int32_t vivek(){
+int t;
+cin>>t;
+while(t-->0)
+    solve();
+return 0;
+}
+
+// ___.   .__                 __          .__              __    
+// \_ |__ |  | _____    ____ |  | _____  _|__|__  __ ____ |  | __
+//  | __ \|  | \__  \ _/ ___\|  |/ /\  \/ /  \  \/ // __ \|  |/ /
+//  | \_\ \  |__/ __ \   \___|    <  \   /|  |\   /\  ___/|    < 
+//  |___  /____(____  /\___  >__|_ \  \_/ |__| \_/  \___  >__|_ \
+//      \/          \/     \/     \/                    \/     \/

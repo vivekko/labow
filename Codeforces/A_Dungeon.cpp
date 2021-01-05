ad6bb578451b1676c5bb8a7a5c868b16a6a97bb8
Vivekko <vivekojha961@gmail.com>
2021-01-05 20:03:56 +0530

    OC

---
+++ Codeforces/A_Dungeon.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;} 
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+#define NFS(nitro) ios_base::sync_with_stdio(false);cin.tie(0);
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+#define int long long
+#define vivek main
+#define all(x) (x).begin(),(x).end()
+#define vec_p vector<pair<int,int>
+#define v vector<int>
+void solve(){
+    int n=3;
+    v a(n);
+    for(int &i:a)   cin>>i;
+    int sum = 0;
+    // int mx = *max_element(all(a));
+    // int mn = *min_element(all(a));
+    for(int i:a)    sum+=i;
+    // debug(sum);
+    // if(sum == 9){
+    //     cout<<"YES\n";
+    //     return;
+    // }
+    int x = sum/9;
+    
+    
+    if(sum%9 == 0 && a[0]!=0 && a[1]!=0 && a[2]!=0 && x<=min(a[0],min(a[1],a[2]))){
+        cout<<"YES\n";
+        return;
+    }
+
+    cout<<"NO\n";
+
+   
+}
+int32_t vivek(){
+NFS(NITRO);
+int t;
+cin>>t;
+while(t-->0)
+    solve();
+return 0;
+}
+
+// ___.   .__                 __          .__              __    
+// \_ |__ |  | _____    ____ |  | _____  _|__|__  __ ____ |  | __
+//  | __ \|  | \__  \ _/ ___\|  |/ /\  \/ /  \  \/ // __ \|  |/ /
+//  | \_\ \  |__/ __ \   \___|    <  \   /|  |\   /\  ___/|    < 
+//  |___  /____(____  /\___  >__|_ \  \_/ |__| \_/  \___  >__|_ \
+//      \/          \/     \/     \/                    \/     \/
+
+// 7,8,9 ,10,11,12,13,14,15,   16,17,18   19 20 21 22 23 24   25,26,27...
---
+++ Codeforces/A_Regular_Bracket_Sequence.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;} 
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+#define NFS(nitro) ios_base::sync_with_stdio(false);cin.tie(0);
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+#define int long long
+#define vivek main
+#define all(x) (x).begin(),(x).end()
+#define vec_p vector<pair<int,int>
+#define v vector<int>
+void solve(){
+    int ans = 0;
+    string a;cin>>a;
+    if(a.size()&1){
+        cout<<"NO\n";
+        return;
+    }
+    int mn = INT32_MAX;
+    int q = 0;
+    for(char c:a){
+        if(c == '('){
+            ans++;
+        mn = min(mn,ans);
+        }
+        else if(c == ')'){
+            ans--;
+            if(q == 0 && ans<0){
+                cout<<"NO\n";
+                return;
+            }
+        mn = min(mn,ans);
+        }
+        else{
+            q++;
+        }
+    }
+    if(a[a.size()- 1] == '('){
+        cout<<"NO\n";
+        return;
+    }
+
+    if(mn == INT32_MAX){
+        if(q%2 == 0)    cout<<"YES\n";
+        else    cout<<"NO\n";
+        return;
+    }
+
+    if(mn == 0 && ans == 0 && q%2 == 0){
+        cout<<"YES\n";
+        return;
+    }
+    if(ans <= q){
+        cout<<"YES\n";
+        
+    }
+    else{
+        cout<<"NO\n";
+    }
+
+}
+int32_t vivek(){
+NFS(NITRO);
+int t;
+cin>>t;
+while(t-->0)
+    solve();
+return 0;
+}
+
+// ___.   .__                 __          .__              __    
+// \_ |__ |  | _____    ____ |  | _____  _|__|__  __ ____ |  | __
+//  | __ \|  | \__  \ _/ ___\|  |/ /\  \/ /  \  \/ // __ \|  |/ /
+//  | \_\ \  |__/ __ \   \___|    <  \   /|  |\   /\  ___/|    < 
+//  |___  /____(____  /\___  >__|_ \  \_/ |__| \_/  \___  >__|_ \
+//      \/          \/     \/     \/                    \/     \/
